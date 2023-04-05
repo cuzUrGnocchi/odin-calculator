a4a4bab72f022602b9b6c26f3f331a7e2c4f3a0f
gnocchi <gkussunoki@gmail.com>
2023-04-05 10:30:18 -0300

    Add buttons for numbers

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const addition = document.querySelector('button.addition');
 const subtraction = document.querySelector('button.subtraction');
 const multiplication = document.querySelector('button.multiplication');
 const division = document.querySelector('button.division');
+const numberButtons = document.querySelectorAll('button.number');
 
 const calculator = {
     memory: 0,
@@ -38,28 +39,38 @@ const calculator = {
     }
 };
 
-document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keydown', numberInputHandler);
 addition.addEventListener('click', operationHandler);
 subtraction.addEventListener('click', operationHandler);
 multiplication.addEventListener('click', operationHandler);
 division.addEventListener('click', operationHandler);
+numberButtons.forEach(button => button.addEventListener('click', numberInputHandler));
 
-function keyDownHandler(event) {
-    if (event.keyCode >= 49 && event.keyCode <= 57) {
-        const input = event.keyCode - 49;
-        
+function numberInputHandler(event) {
+    let input;
+    if (event.type === 'keydown') {
+        if (event.keyCode >= 49 && event.keyCode <= 57) {
+            input = event.keyCode - 49;
+        } else if (event.keyCode === 190) { // e.g. dot
+            input = '.';
+        }
+    } else if (event.type === 'click') {
+        input = +this.textContent;
+    }
+
+    if (typeof input === 'number') {
         if (display.textContent.length >= 9) return;
         if (input === 0 && display.textContent === '0') return;
-
+        
         if (display.textContent === '0' || calculator.waitingForNewOperand) {
             calculator.waitingForNewOperand = false;
             display.textContent = input;
         } else {
             display.textContent += input; 
         }
-    } else if (event.keyCode === 190) { // e.g. dot
+    } else if (input === '.') {
         if (display.textContent.includes('.')) return;
-
+        
         display.textContent += '.';
     }
 }
