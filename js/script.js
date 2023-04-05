90c7a0e2f2d3c8bdc18c5af79f21cae24c2a697a
gnocchi <gkussunoki@gmail.com>
2023-04-05 09:28:07 -0300

    Add buttons for arithmetic operations

--- js/script.js
+++ js/script.js
@@ -1,41 +1,72 @@
+const display = document.querySelector('span.display');
+const addition = document.querySelector('button.addition');
+const subtraction = document.querySelector('button.subtraction');
+const multiplication = document.querySelector('button.multiplication');
+const division = document.querySelector('button.division');
+
 const calculator = {
     memory: 0,
-    waitingForNewOperator: false,
-    add(val) {
-        this.memory += val;
-        this.waitingForNewOperator = true;
-        return this.memory;
-    },
-    subtract(val) {
-        this.memory -= val;
-        this.waitingForNewOperator = true;
-        return this.memory;
-    },
-    multiply(val) {
-        this.memory *= val;
-        this.waitingForNewOperator = true;
-        return this.memory;
-    },
-    divide(val) {
-        this.memory /= val;
-        this.waitingForNewOperator = true;
+    lastOperator: null,
+    waitingForNewOperand: false,
+    evaluate(operator, val) {
+        if (this.lastOperator === null) {
+            this.memory = val;
+        } else {
+            switch (this.lastOperator) {
+                case '+':
+                    this.memory += val;
+                    break;
+                case '-':
+                    this.memory -= val;
+                    break;
+                case '*':
+                    this.memory *= val;
+                    break;
+                case '/':
+                    this.memory /= val;
+                    break;
+            }
+        }
+        this.waitingForNewOperand = true;
+        this.lastOperator = operator;
         return this.memory;
     },
     clear() {
         this.memory = 0;
-        this.waitingForNewOperator = false;
+        this.waitingForNewOperand = false;
         return this.memory;
     }
 };
 
-const display = document.querySelector('.display');
 document.addEventListener('keydown', keyDownHandler);
+addition.addEventListener('click', operationHandler);
+subtraction.addEventListener('click', operationHandler);
+multiplication.addEventListener('click', operationHandler);
+division.addEventListener('click', operationHandler);
 
 function keyDownHandler(event) {
-    if (event.keyCode < 49 || event.keyCode > 57) return;
-    if (display.textContent.length >= 9) return;
-    if (event.keyCode === 49 && +display.textContent === 0) return;
+    if (event.keyCode >= 49 && event.keyCode <= 57) {
+        const input = event.keyCode - 49;
+        
+        if (display.textContent.length >= 9) return;
+        if (input === 0 && display.textContent === '0') return;
 
-    const number = event.keyCode - 49;
-    display.textContent += number;
+        if (display.textContent === '0' || calculator.waitingForNewOperand) {
+            calculator.waitingForNewOperand = false;
+            display.textContent = input;
+        } else {
+            display.textContent += input; 
+        }
+    } else if (event.keyCode === 190) { // e.g. dot
+        if (display.textContent.includes('.')) return;
+
+        display.textContent += '.';
+    }
+}
+
+function operationHandler(event) {
+    if (calculator.waitingForNewOperand) return;
+    const numberInDisplay = +display.textContent;
+    const operator = this.dataset.operator;
+    display.textContent = calculator.evaluate(operator, numberInDisplay);
 }
